c223543ae0912a7b1bd7f052148e6d38019d7e8f
Ariel Villafane <ariel.g.villafane@gmail.com>
2021-12-11 16:25:14 -0300

    actualizado solo  dos comments

--- bucleador.py
+++ bucleador.py
@@ -1,14 +1,2 @@
 #dos lineas agregadas especificamente
 #para un nuevo branch en git . no afecta en nada al codigo
-def run():
-    contador = 0
-    while contador < 1000 :
-        print ('2 elevado a ' + str(contador) + ' es igual a ' + str(2 ** contador))
-        contador = contador + 1
-    
-
-
-if __name__ == '__main__':
-    run()
-    
-    #esta linea es agregada via GitHub
