90a6a20bffb86dd3dddfca822fe19e4af74c0671
Ariel Villafane <ariel.g.villafane@gmail.com>
2021-12-11 16:12:17 -0300

    proyecto bucleador

---
+++ bucleador.py
@@ -0,0 +1,10 @@
+def run():
+    contador = 0
+    while contador < 1000 :
+        print ('2 elevado a ' + str(contador) + ' es igual a ' + str(2 ** contador))
+        contador = contador + 1
+    
+
+
+if __name__ == '__main__':
+    run()
